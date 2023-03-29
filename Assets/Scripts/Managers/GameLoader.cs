d1d24ccb37761bff9d8a8123debe58348a264c7f
Furkan YÜKSEL <furkan1928yuksel@gmail.com>
2023-03-29 22:03:28 +0300

    Add : loader scene

---
+++ Assets/Scripts/Managers/GameLoader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameLoader : MonoBehaviour
+{
+    [SerializeField] GameObject loadingScreen;
+    [SerializeField] Slider slider;
+    public void LoadGame(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        loadingScreen.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            yield return null;
+        }
+   }
+
+
+}
