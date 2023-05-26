15ba5332e6129c254bbfff8a0463c7e5f794115f
Furkan YÜKSEL <81265340+furkanyuksell@users.noreply.github.com>
2023-05-26 14:05:15 +0300

    Update RawMaterialPool.cs

--- Assets/Test/RawMaterialPool.cs
+++ Assets/Test/RawMaterialPool.cs
@@ -7,7 +7,6 @@ using UnityEngine.UIElements;
 public class RawMaterialPool: MonoBehaviour
 {
     public static RawMaterialPool Instance { get; private set; }
-    private Dictionary<RawMaterial, ObjectPooler<RawMaterial>> pools = new();
     private Dictionary<Tuple<Region, RawMaterial>, ObjectPooler<RawMaterial>> _rawPools = new();
     private void Awake()
     {
@@ -16,12 +15,6 @@ public class RawMaterialPool: MonoBehaviour
 
     public void InitRawMaterialPools(Region region, RawMaterial rawMaterial, Transform regionParent)
     {
-        /*
-        if (!pools.ContainsKey(rawMaterial))
-        {
-            pools.Add(rawMaterial, new ObjectPooler<RawMaterial>(rawMaterial.gameObject, regionParent));   
-        }*/
-
         var keyToCheck = new Tuple<Region, RawMaterial>(region, rawMaterial);
         if (!_rawPools.ContainsKey(keyToCheck))
         {
@@ -37,26 +30,13 @@ public class RawMaterialPool: MonoBehaviour
             rawMaterialType.activeCount = rawPool.Pool.CountActive+1;
             return rawPool.Pool.Get();
         }
-        /*
-        if (pools.TryGetValue(rawMaterialType.rawMaterial, out ObjectPooler<RawMaterial> pool))
-        {
-            rawMaterialType.activeCount = pool.Pool.CountActive+1;
-            return pool.Pool.Get();
-            /*Debug.Log(
-                "RawMaterial: " +
-                rawMaterial.name +
-                " / Active: " +
-                pool.Pool.CountActive
-                + " / Inactive: " +
-                pool.Pool.CountInactive
-            );
-        }*/
+        
         return null;
     }
 
     public int GetActiveRawMaterialCount(Region region, TileData.RawMaterialType rawMaterialType)
     {
         return _rawPools.TryGetValue(new Tuple<Region, RawMaterial>(region, rawMaterialType.rawMaterial), out ObjectPooler<RawMaterial> rawPool) ? rawPool.Pool.CountActive : 0;
-        //return pools.TryGetValue(rawMaterialType.rawMaterial, out ObjectPooler<RawMaterial> pool) ? pool.Pool.CountActive : 0;
+        
     }
 }
